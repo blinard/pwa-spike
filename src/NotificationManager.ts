2b49dde35db4b7b09669433f4b386cbdaa25341c
bradlinard <brad.linard@idelata.com>
2018-01-22 00:32:56 -0800

    initial pwa working with local notification

---
+++ src/NotificationManager.ts
@@ -0,0 +1,30 @@
+// tslint:disable-next-line:one-variable-per-declaration
+declare let Notification: any;
+
+export class NotificationManager {
+    constructor() { }
+
+    public showNotification(title: string, text: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (!Notification || Notification.permission === "denied") {
+                reject();
+                return;
+            }
+
+            if (Notification.permission === "granted") {
+                const blah = new Notification(title, { body: text });
+                resolve();
+                return;
+            }
+
+            return Notification.requestPermission((permission: string) => {
+                if (permission === "granted") {
+                    const blah2 = new Notification(title, { body: text });
+                    return resolve();
+                }
+
+                return reject();
+            });
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
+import { NotificationManager } from "./NotificationManager.js";
 
 function Bootstrap(): void {
-    // do something
+    window.onload = () => {
+        const index = new Index(new NotificationManager());
+        index.load();
+    };
 }
 
 Bootstrap();
+
+export class Index {
+    constructor(private notificationManager: NotificationManager) { }
+
+    public load(): void {
+        const btnShowNotification = document.getElementById("btnShowNotification");
+        if (!btnShowNotification) return;
+        btnShowNotification.onclick = this.btnShowNotification_onClick.bind(this);
+    }
+
+    private btnShowNotification_onClick(): void {
+        this.notificationManager.showNotification("Yo", "Wazzup?!");
+    }
+
+}
